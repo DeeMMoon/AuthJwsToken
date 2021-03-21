cd0177f72f64cd2baf2b7dfef292908dccb4070d
DeeMMoon <37477667+DeeMMoon@users.noreply.github.com>
2021-03-21 04:43:20 +0300

    Add files via upload

---
+++ demoauth/src/main/java/com/example/demoauth/pojo/SignUpRequest.java
@@ -0,0 +1,14 @@
+package com.example.demoauth.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+@Getter @Setter
+public class SignUpRequest {
+
+    private String username;
+    private String email;
+    private String password;
+    private Set<String> role;
+}
